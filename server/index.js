1bdfaf37fa635c45cea3f5e3f7a9d072602a4c4d
agus_prayogi <agusyogi600@gmail.com>
2020-06-01 12:39:57 +0700

    add server

---
+++ server/index.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const socket = require('socket.io')
+const root = require('./router')
+const http = require('http')
+const cors = require('cors')
+
+var app = express()
+var port = process.env.PORT || 3000
+var server = http.createServer(app)
+
+var io = socket(server)
+app.use(express.static('public'))
+app.use(root.router)
+app.use(cors)
+
+let interval
+
+io.on('connection', (socket) => {
+  console.log(`New client ${socket.id} connected`)
+  if (interval) {
+    clearInterval(interval)
+  }
+  interval = setInterval(() => getApiAndEmit(socket), 1000)
+  // socket.on('getUser', function (params) {})
+  socket.on('disconnect', () => {
+    console.log(`Client ${socket.id} disconnected`)
+    clearInterval(interval)
+  })
+})
+
+const getApiAndEmit = (socket) => {
+  const response = new Date()
+  // Emitting a new message. Will be consumed by the client
+  socket.emit('FromAPI', response)
+}
+
+server.listen(port, function () {
+  console.log(`Listening ${port}`)
+})
